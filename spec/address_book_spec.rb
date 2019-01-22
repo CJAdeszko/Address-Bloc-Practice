419ed0d365e87d8bcfdd9698dafd667a816621f9
Christopher Adeszko <cjadeszko@gmail.com>
2019-01-22 11:43:49 -0700

    Implement remove_entry method and specs

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,5 +36,23 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
-  
+
+  describe '#remove_entry' do
+    it 'removes only one entry from the address book' do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      name = 'Test User'
+      phone_number = '123.456.7890'
+      email = 'testuser@test.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Test User')
+    end
+  end
+
 end
